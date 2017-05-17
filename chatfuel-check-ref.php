002627b3b2ffea0f93e93fcecd6a3d1c184f4836
Alex P <alexander@lanos.co.uk>
2017-05-17 23:15:01 +0300

    chatbot files

---
+++ chatfuel-check-ref.php
@@ -0,0 +1,45 @@
+<?php
+
+error_reporting(0);
+
+$servername = "109.169.61.112";
+$username = "chatfuel";
+$password = "unh4ck4bl3123!";
+$dbname = "chatfuel";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+	
+$read_sql = "select status from records where id=".$_GET['reference']." limit 0,1";
+$result = $conn->query($read_sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		 echo '
+{
+ "messages": [
+   {"text": "The status for reference '.$_GET['reference'].' is `'.$row["status"].'` You can come back here later to lookup the status of your issue using this number."}
+ ]
+}
+';
+}
+
+} 
+else 
+{
+		 echo '
+{
+ "messages": [
+   {"text": "Unable to retrieve the reference number at this point"}
+ ]
+}
+';
+}
+$conn->close();
+
+?>
---
+++ chatfuel.php
@@ -0,0 +1,56 @@
+<?php
+
+error_reporting(0);
+
+//file_put_contents('test.txt', json_encode($_GET)); // save the JSON data passed on to URL
+$json_result = json_encode($_GET);
+
+$servername = "109.169.61.112";
+$username = "chatfuel";
+$password = "unh4ck4bl3123!";
+$dbname = "chatfuel";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO records (id, result) VALUES (NULL, '$json_result')";
+
+$conn->query($sql);
+//$conn->close();
+
+// select the reference number and display to the user
+	
+$read_sql = "select max(id) as reference from records limit 0,1";
+$result = $conn->query($read_sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        // echo 'Your reference number is '.$row["reference"].'!"}';
+		 echo '
+{
+ "messages": [
+   {"text": "Thank you for submitting this information. Your reference number is '.$row["reference"].'. You can come back here and lookup the status of your issue using this number."}
+ ]
+}
+';
+}
+
+} 
+else 
+{
+		 echo '
+{
+ "messages": [
+   {"text": "Unable to retrieve the reference number at this point"}
+ ]
+}
+';
+}
+$conn->close();
+
+?>
