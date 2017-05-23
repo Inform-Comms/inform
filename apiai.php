8a12721c09cc40ca37bb71ecc177a7c528e542c4
Alex P <alexander@lanos.co.uk>
2017-05-23 16:33:09 +0300

    Trying POST for API

--- apiai.php
+++ apiai.php
@@ -16,9 +16,62 @@ $result =$response->result;  //result is an array
 $parameters = $result->parameters;  // parameters is also an array
 
 
-//if (array_key_exists('reference_number', $parameters)) {
-    $speech=$response->result->parameters->reference_number;
-//}
+
+
+
+
+
+
+
+
+
+// Check if reference number exists in the database
+if (array_key_exists('reference_number', $parameters)) {
+    
+$reference_number = $response->result->parameters->reference_number;
+
+$read_sql = "select status from records where id=".$reference_number." limit 0,1";
+$result = $conn->query($read_sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+     $speech='The status for reference '.$reference_number.' is `'.$row["status"].'` You can come back here later to lookup the status of your issue using this number.';
+}
+
+} 
+else 
+{
+    $speech="Unable to retrieve the reference number at this point";
+}
+$conn->close();
+
+
+
+
+//outputting back to api.ai - works fine as well
+header('Content-Type: application/json');
+
+$context ="report-issue";
+$source= "Inform123";
+
+
+$json = array(
+                    'speech'   => $speech,
+                    'displayText' => $text,
+                    'source' => $source
+            );
+
+// $json = array(
+//                    'speech'   => $speech,
+//                    'displayText' => $text,
+//                    'data' => [],
+//                    'contextOut' => [$context],
+//                    'source' => $source
+//            );
+  echo json_encode($json, JSON_PRETTY_PRINT);
+
+}
 
 
 
