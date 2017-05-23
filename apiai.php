051276e8b81b40b6f1a01d7daaafe4c46741e33c
Alex P <alexander@lanos.co.uk>
2017-05-23 16:07:37 +0300

    Trying POST for API

--- apiai.php
+++ apiai.php
@@ -1,5 +1,10 @@
 <?php
 
+$servername = "us-cdbr-iron-east-03.cleardb.net";
+$username = "bc8adfd337a147";
+$password = "8636ab6c";
+$dbname = "heroku_6b42f3a320e7b6f";
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 $json_data = file_get_contents("php://input");
@@ -17,8 +22,56 @@ $speech=  $response->result->parameters->address;
 $text=$speech;
 }
 
-$speech= "Not sure what to say next";
-$text= "Not sure what to say next";
+
+
+
+
+
+// Create connection. Writing into DB at this block
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO records (id, result) VALUES (NULL, '$json_data')";
+
+$conn->query($sql);
+
+
+
+
+
+
+
+
+
+
+// select the reference number and display to the user
+  
+$read_sql = "select max(id) as reference from records limit 0,1";
+$result = $conn->query($read_sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        // echo 'Your reference number is '.$row["reference"].'!"}';
+     $speech= 'Thank you for submitting this information. Your reference number is '.$row["reference"].'. You can come back here and lookup the status of your issue using this number.';
+}
+
+} 
+else 
+{
+          $speech= 'Unable to retrieve the reference number at this point';
+}
+$conn->close();
+
+
+$text = $speech;
+
+
+//$speech= "Not sure what to say next";
+//$text= "Not sure what to say next";
 
 
 
