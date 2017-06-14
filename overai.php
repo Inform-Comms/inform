ad0becf6d5f5d7ef6dbc69538e597d8a45a207bd
Alex P <alexander@lanos.co.uk>
2017-06-14 18:20:05 +0300

    Modified Over.ai script

--- overai.php
+++ overai.php
@@ -16,17 +16,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
         $json_data = file_get_contents("php://input");
         $response=json_decode($json_data);
-        print_r($response->nluResponse);    
+        //print_r($response->nluResponse);    
         //$reference_number ='123';
         $reference_number = $response->nluResponse->input;
 
- echo  
-     '{
-  "result": {
-  "introSpeakOut": "18:17 The status for reference '.$reference_number.' whatever"
-  }
-}';
-exit(0);
+        // get the reference number status from the database
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+          
+        $read_sql = "select status from records where id=".$reference_number." limit 0,1";
+        $result = $conn->query($read_sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+             echo  
+             '{
+          "result": {
+          "introSpeakOut": "18:20 The status for reference '.$reference_number.' is `'.$row["status"].'` You can come back here later to lookup the status of your issue using this number."
+          }
+        }';
+        }
+
+        } 
+        else 
+        {
+             echo 
+
+        '
+        {
+          "result": {
+          "introSpeakOut": "Unable to retrieve the reference number at this point"
+          }
+        }';
+
+
+        ;
+        }
+$conn->close();
 
 }
 else
@@ -52,44 +83,7 @@ exit(0);
 
 
 
-// get the reference number status from the database
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-  
-$read_sql = "select status from records where id=".$_GET['reference']." limit 0,1";
-$result = $conn->query($read_sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-     echo  
-     '{
-  "result": {
-  "introSpeakOut": "The status for reference '.$_GET['reference'].' is `'.$row["status"].'` You can come back here later to lookup the status of your issue using this number."
-  }
-}';
-}
-
-} 
-else 
-{
-     echo 
-
-'
-{
-  "result": {
-  "introSpeakOut": "Unable to retrieve the reference number at this point"
-  }
-}';
-
-
-;
-}
-$conn->close();
 
 
 
